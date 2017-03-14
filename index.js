2c3f8f0486dd9893295fbc76c19cb949f681503f
Ali AbuRas <Ali.AbuRas@mbc.net>
2017-03-14 15:47:38 +0200

    update server

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id:1,
+    description: 'Jana and Joud',
+    completed:false
+  },
+  {
+    id:2,
+    description:'macbook',
+    completed:false
+  },
+  {
+    id:3,
+    description:'mbc',
+    completed:true
 
+  }];
 
 app.get('/', function(req,res){
   res.send('todo API ROOT');
+});
+
+app.get('/todos',function(req,res){
+  res.json(todos);
+});
+
+app.get('/todos/:id',function(req,res){
+  var todoID = req.params.id;
+  todos.forEach(function(todo){
+    if(todo.id == todoID){
+      res.json(todo);
+      return;
+    }
+  });
+
+  res.send(todoID+', there is no such ID in our database!');
 });
 
 app.listen(PORT,function(){
