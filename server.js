cf71aa737db4e9eb47ed3835433b9ac2cee4b807
Ali AbuRas <Ali.AbuRas@mbc.net>
2017-03-16 15:21:10 +0200

    add filter method

--- server.js
+++ server.js
@@ -16,7 +16,20 @@ app.get('/', function(req,res){
 });
 
 app.get('/todos',function(req,res){
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if(queryParams.hasOwnProperty('completed') &&
+     queryParams.completed === 'true'){
+
+    filteredTodos = _.where(filteredTodos,{completed:true});
+  } else if(queryParams.hasOwnProperty('completed') &&
+            queryParams.completed === 'false'){
+
+    filteredTodos =_.where(filteredTodos, {completed : false});
+  }
+  res.json(filteredTodos);
+
 });
 
 app.get('/todos/:id',function(req,res){
@@ -104,8 +117,6 @@ app.put('/todos/:id',function(req,res){
       return res.status(400).send({"error":'its not string'});
     }
   }
-
-
   _.extend(matchedTodo, validAttributes);
   res.json(matchedTodo);
 });
